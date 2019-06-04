e14732fa74a8815cfc0fa1a3a254d78e5c1f502c
lakshman, shiva <lakshman.shiva@aspiresys.com>
2019-06-04 23:44:50 +0530

    Lodable component added

--- src/main/java/com/flighcenter/pages/BasePage.java
+++ src/main/java/com/flighcenter/pages/BasePage.java
@@ -1,16 +1,16 @@
 package com.flighcenter.pages;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.LoadableComponent;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Base Page class -  Super class of all Page classes.
  * @author lakshman.shiva
+ * @param <T>
  * @since  03-June-2019
  */
-public class BasePage {
+public class BasePage <T extends LoadableComponent<T>> extends LoadableComponent<T>{
 
 private static final int TIMEOUT = 5;
 private static final int POLLING = 100;
@@ -19,8 +19,21 @@ protected WebDriverWait wait;
 
 public BasePage(WebDriver driver){	
 	this.driver = driver;	
-	wait = new WebDriverWait(driver, TIMEOUT, POLLING);	
-	PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+	wait = new WebDriverWait(driver, TIMEOUT, POLLING);		
 }
+
+@Override
+protected void isLoaded() throws Error {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+protected void load() {
+	// TODO Auto-generated method stub
+	
+}
+
+
 	
 }
